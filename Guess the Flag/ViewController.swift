ee61794a21da1c10c13ca6bb57df10af5137e84a
AKBarcenas <akbarcen@ucsd.edu>
2015-12-20 15:51:23 -0800

    Added functionality to button presses

--- Guess the Flag/ViewController.swift
+++ Guess the Flag/ViewController.swift
@@ -35,13 +35,31 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
         countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
         button1.setImage(UIImage(named: countries[0]), forState: .Normal)
         button2.setImage(UIImage(named: countries[1]), forState: .Normal)
         button3.setImage(UIImage(named: countries[2]), forState: .Normal)
         correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
         title = countries[correctAnswer].uppercaseString
+    }
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            ++score
+        }
+            
+        else {
+            title = "Wrong"
+            --score
+        }
+        
+        let ac = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .Default, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
